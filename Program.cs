0f6ade61e7e44b622b583a950225a6c0720d0bb6
elifkuus <elifkuus@gmail.com>
2021-12-01 01:12:40 +0300

    Update

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace CSharp_Programming
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("İsim giriniz: ");
+            string name=Console.ReadLine();
+
+            Console.WriteLine("isminiz: "+name);
+        }
+    }
+}
