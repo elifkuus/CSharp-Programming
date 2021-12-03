575e5e9f7588ef0a9b25904c5578dd8634310e33
elifkuus <elifkuus@gmail.com>
2021-12-04 00:35:31 +0300

    While-Foreach Loop

---
+++ While-ForeachLoop/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace While_ForeachLoop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // While
+            //1 den baslayarak konsoldan girilen sayiya kadar ort hesaplayip yazdir
+
+            Console.WriteLine("Bir sayi giriniz: ");
+            int sayi = int.Parse(Console.ReadLine());
+
+            int sayac = 1;
+            int toplam = 0;
+
+            while (sayac <= sayi)
+            {
+                toplam += sayac;
+                sayac++;
+            }
+            Console.WriteLine(toplam / sayi);
+
+            // a dan z ye kadar tum harfleri konsola yazdir
+            char character = 'a';
+            while (character <= 'z')
+            {
+                Console.Write(character);
+                character++;
+            }
+            Console.WriteLine();
+            
+            Console.WriteLine("******* Foreach ********");
+            string[] arabalar = { "BMW", "Ford", "Toyota", "Nissan" };
+
+            foreach (var araba in arabalar)
+            {
+                Console.WriteLine(araba);
+            }
+        }
+    }
+}
