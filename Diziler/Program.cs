bbdbd67992d12e623c8c02a0c95a98af415f8417
elifkuus <elifkuus@gmail.com>
2021-12-05 00:21:27 +0300

    Diziler Load

---
+++ Diziler/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Diziler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           //dizi tanimlama
+           string[] renkler=new string[5];
+           
+           string[] hayvanlar={"kedi","kopek","kus","maymun"};
+
+           int[] dizi;
+           dizi=new int[5];
+
+           //dizilere deger atama
+           renkler[0]="mavi";
+           dizi[3]=10;
+
+           Console.WriteLine(hayvanlar[1]);
+           Console.WriteLine(dizi[3]);
+           Console.WriteLine(renkler[0]);
+
+           //dongulerle dizi kullanimi
+
+           Console.WriteLine("Dizi uzunlugu giriniz: ");
+           int diziUzunlugu=int.Parse(Console.ReadLine());
+           int[] sayiDizisi=new int[diziUzunlugu];
+
+           for (int i = 0; i < diziUzunlugu; i++)
+           {
+               Console.Write("{0}. sayisini giriniz");
+               sayiDizisi[i]=int.Parse(Console.ReadLine());
+           }
+
+           int toplam=0;
+           foreach (var item in sayiDizisi)
+           {
+              toplam+=item;
+           }
+            Console.WriteLine("ortalama: "+toplam/diziUzunlugu);
+        }
+    }
+}
