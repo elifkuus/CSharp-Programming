c72af8a29d6a4060238961654c5d52a792d1b557
elifkuus <elifkuus@gmail.com>
2021-12-07 20:30:56 +0300

    uyg2

--- Odev1/Uyg2/Program.cs
+++ Odev1/Uyg2/Program.cs
@@ -6,7 +6,26 @@ namespace Uyg2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Pozitif birinci sayiyi giriniz: ");
+            int n=int.Parse(Console.ReadLine());   
+            Console.WriteLine("Pozitif ikinci sayiyi giriniz: ");
+            int m=int.Parse(Console.ReadLine());                 
+
+            int[] dizi=new int[n];
+            
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine("Pozitif bir sayi giriniz: ");
+                dizi[i]=int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine("Girilen sayilardan m'ye esit veya tam bolunenler: ");
+            foreach (var item in dizi)
+            {
+                if (m==item || item%m==0)
+                {
+                    Console.Write(item+" ");
+                }
+            }
         }
     }
 }
