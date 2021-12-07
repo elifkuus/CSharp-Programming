9a60accacb634b4cca98fa74ccdf8f3e4fb602d7
elifkuus <elifkuus@gmail.com>
2021-12-07 18:12:27 +0300

    Metot overloading ve out parametreler

---
+++ Metot-Overloading/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace Metot_Overloading
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //out parametreler
+
+            string sayi="999";
+
+            bool sonuc=int.TryParse(sayi,out int outSayi);
+            if (sonuc==true)
+            {
+                Console.WriteLine("Basarili!");
+                Console.WriteLine(outSayi);
+            }
+            else
+            {
+                Console.WriteLine("Basarisiz!");               
+            }
+
+            Metotlar instance=new Metotlar();
+            instance.Topla(4,5,out int toplamSonucu);
+            Console.WriteLine(toplamSonucu);
+
+            //Metot Asiri Yukleme
+
+            int ifade=999;
+            instance.EkranaYaz(ifade.ToString());
+            instance.EkranaYaz(ifade);
+            instance.EkranaYaz("elif","kus");
+
+
+        }
+    }
+    class Metotlar{
+
+        public void Topla(int a,int b,out int toplam)
+        {
+            toplam=a+b;
+        }
+        public void EkranaYaz(string veri)
+        {
+            Console.WriteLine(veri);
+            
+        }
+        public void EkranaYaz(int veri)
+        {
+            Console.WriteLine(veri);
+
+        }
+        public void EkranaYaz(string veri1,string veri2)
+        {
+            Console.WriteLine(veri1+veri2);
+
+        }
+
+    }
+}
