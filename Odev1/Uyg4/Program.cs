2480e94a0163f257e1f66aad2b90d60080c23b54
elifkuus <elifkuus@gmail.com>
2021-12-07 21:22:26 +0300

    uyg4

--- Odev1/Uyg4/Program.cs
+++ Odev1/Uyg4/Program.cs
@@ -6,7 +6,36 @@ namespace Uyg4
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Bir cumle yaziniz: ");
+            string n=Convert.ToString(Console.ReadLine());     
+           
+            Console.WriteLine("Toplam kelime sayisi: "+n.WordCount());
+            Console.WriteLine("Toplam harf sayisi: "+n.LetterCount());              
+
+
+        }
+    }
+    public static class Extension
+    {
+        public static int WordCount(this string param)
+        {
+            string[] dizi=param.Split(" ");
+            return dizi.Length;
+        }
+        public static int LetterCount(this string param)
+        {
+            string liste =  "ABCÇDEFGĞHİIJKLMNOÖPRSŞTUÜVYZ"
+                           +"abcçdefgğhiıjklmnoöprsştuüvyz";
+            int sayac=0;
+            for (int i = 0; i < param.Length; i++)
+            {
+               if (liste.Contains(param[i]))
+               {
+                   sayac++;
+               }
+            }
+            return sayac;
+
         }
     }
 }
