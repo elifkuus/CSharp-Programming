5e7179613deb0cecf21c1d23529cd37c49cf37fc
elifkuus <elifkuus@gmail.com>
2021-12-07 20:26:16 +0300

    uyg1

--- Odev1/Uyg1/Program.cs
+++ Odev1/Uyg1/Program.cs
@@ -6,7 +6,26 @@ namespace Uyg1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Pozitif bir sayi giriniz: ");
+            int n=int.Parse(Console.ReadLine());                     
+
+            int[] cift=new int[n];
+            
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine("Pozitif bir sayi giriniz: ");
+                cift[i]=int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine("Cift Sayilar: ");
+            foreach (var item in cift)
+            {
+                if (item%2==0)
+                {
+                    Console.Write(item+" ");
+                }
+            }
+            
+
         }
     }
 }
