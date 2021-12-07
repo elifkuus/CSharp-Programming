9ab8de0931dd02082083a6bbf99e8ae80cf5614e
elifkuus <elifkuus@gmail.com>
2021-12-07 19:06:44 +0300

    Odev 1

---
+++ Odev1/Uyg4/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Uyg4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
